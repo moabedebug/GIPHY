281d0ed0a2b8400e8ce33597d9dcdd44d544445a
Moabe <moabeluis2018@gmail.com>
2022-07-20 20:56:53 -0300

    Background Screen Details

--- src/screens/Details.js
+++ src/screens/Details.js
@@ -1,19 +1,22 @@
 import React from 'react'
-import { View, Text, StyleSheet, Button, TouchableOpacity } from "react-native"
+import { View, Text, StyleSheet, ImageBackground } from "react-native"
 
 export default function Details({ route }) {
     const data = route.params.item
     return (
-        <View style={Styles.container}>
-            <Text> Details </Text>
-        </View>
+        <ImageBackground
+            source={require("../../assets/BG.png")}
+            style={Styles.container}
+        >
+            <View style={Styles.}>
+                <Text> Details </Text>
+            </View>
+        </ImageBackground>
     )
 }
 
 const Styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
     },
 })
