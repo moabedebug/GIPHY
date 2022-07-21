519a3c8eee46f18f7f0906dac6e12d9db8849298
Moabe <moabeluis2018@gmail.com>
2022-07-20 21:02:28 -0300

    Adding back screen action

--- src/screens/Details.js
+++ src/screens/Details.js
@@ -1,14 +1,28 @@
 import React from 'react'
-import { View, Text, StyleSheet, ImageBackground } from "react-native"
+import { 
+    View,
+    Text,
+    StyleSheet,
+    ImageBackground,
+    StatusBar
+ } from "react-native"
 
-export default function Details({ route }) {
+import { Ionicons } from "react-native-vector-icons"
+
+export default function Details({  navigation, route }) {
     const data = route.params.item
     return (
         <ImageBackground
             source={require("../../assets/BG.png")}
             style={Styles.container}
         >
-            <View style={Styles.}>
+            <View style={Styles.a}>
+                <Ionicons
+                    name="chevron-back"
+                    size={40}
+                    color="white"
+                    onPress={() => navigation.navigate("Result")}
+                />
                 <Text> Details </Text>
             </View>
         </ImageBackground>
@@ -18,5 +32,7 @@ export default function Details({ route }) {
 const Styles = StyleSheet.create({
     container: {
         flex: 1,
+        paddingTop: StatusBar.currentHeight,
+        
     },
 })
